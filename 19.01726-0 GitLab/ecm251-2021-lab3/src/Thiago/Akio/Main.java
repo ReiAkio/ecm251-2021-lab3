25216ad5cc683b3a9a4838818ba60a89c72f6397
ReiAkio <tanaka1557@gmail.com>
2021-03-18 09:14:15 -0300

    Atualização Aula05-Teoria

---
+++ 19.01726-0 GitLab/ecm251-2021-lab3/src/Thiago/Akio/Main.java
@@ -0,0 +1,9 @@
+package Thiago.Akio;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    String cpf = "452.037.758-77";
+        System.out.println("Cpf valido: "+ ValidadorCPF.validarCPF(cpf));
+    }
+}
