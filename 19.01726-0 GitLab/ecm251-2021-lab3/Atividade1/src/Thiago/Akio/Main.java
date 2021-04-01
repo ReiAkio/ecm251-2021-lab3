c7281f31c4ece489d407ff7534432b10ad87cb9b
ReiAkio <tanaka1557@gmail.com>
2021-04-01 16:23:57 -0300

    Implementado Switch case para as opções de menu, assim como permitir que o usuario escreva seu nome e senha para que seja logado

--- 19.01726-0 GitLab/ecm251-2021-lab3/Atividade1/src/Thiago/Akio/Main.java
+++ 19.01726-0 GitLab/ecm251-2021-lab3/Atividade1/src/Thiago/Akio/Main.java
@@ -10,6 +10,7 @@ public class Main {
     public static void main(String[] args) {
 
         Sistema.start();
-        Sistema.logarUsuario("Maria","Maria123");
+        Sistema.logarUsuario();
+
     }
 }
--- 19.01726-0 GitLab/ecm251-2021-lab3/Atividade1/src/Thiago/Akio/Sistema.java
+++ 19.01726-0 GitLab/ecm251-2021-lab3/Atividade1/src/Thiago/Akio/Sistema.java
@@ -4,35 +4,94 @@ package Thiago.Akio;
 import java.util.Scanner;
 
 public class Sistema {
+    private boolean continuarExecucao;
+    private Contas conta;
     private static Usuarios usuarioLogado;
     private static Scanner scanner = new Scanner(System.in);
 
 
     public static void start(){
-        new Usuarios("Joao", "Joao", "Joao@",200,111);
+        new Usuarios("Joao", "Joao123", "Joao@",200,111);
         new Usuarios("Guilherme", "Guilherme123", "Guilherme@", 300,222);
         new Usuarios("Maria", "Maria123", "Maria@",500,333);
     }
-    public static void logarUsuario(String nome, String senha){
-
+    public static void logarUsuario(){
+        System.out.println("Qual o seu nome?");
+        String nome = scanner.nextLine();
+        System.out.println("Qual a sua senha?");
+        String senha = scanner.nextLine();
             for (Usuarios usuario : Usuarios.usuariosListados) {
 
-                if (nome.equals(usuario.nome)){
-                    System.out.println("Encontrei !");
+                if (nome.equals(usuario.nome)) {
 
-                        if (senha.equals(usuario.senha)){
+                    if (senha.equals(usuario.senha)) {
 
-                            System.out.println("Vc foi logado");
-                        }
+                        System.out.println("Vc foi logado");
+                        Sistema sistema = new Sistema();
+                        sistema.executar();
+                    }
 
-                }
-                else{
-                    System.out.println("Não foi possivel encontrar um nome em nosso banco de dados");
                 }
 
 
             }
         }
+
+    public void executar(){
+        this.continuarExecucao = true;
+        while(continuarExecucao) {
+            exibirMenu();
+            int opcao = scanner.nextInt();
+            Menu(opcao);
+        }
+    }
+
+    private void Menu (int opcao) {
+        switch (opcao){
+            case 0:
+                System.out.println("Obrigado pela preferencia");
+                this.continuarExecucao = false;
+                break;
+
+            case 1:
+                System.out.println("saldo:"+ conta.getSaldo());
+                this.continuarExecucao = false;
+                break;
+
+            case 2:
+                System.out.println("Digite o valor:");
+                double valordepositado = scanner.nextDouble();
+                conta.depositar(valordepositado);
+                this.continuarExecucao = false;
+                break;
+
+            case 3:
+                System.out.println("Digite o valor:");
+                double valorsacado = scanner.nextDouble();
+                conta.sacar(valorsacado);
+                this.continuarExecucao = false;
+                break;
+
+            /*case 4:
+                System.out.println("Digite o valor transferido:");
+                double valortransferido = scanner.nextDouble();
+                Transacoes.transferirDinheiro(Usuarios.usuariosListados[0], Usuarios.usuariosListados[1], valortransferido );*/
+
+            default:
+                System.out.println("Funcionalidade ainda não implementada");
+                break;
+        }
+    }
+
+    private void exibirMenu() {
+        System.out.println("Bem vindo ao banco");
+        System.out.println("1 - Visualizar Saldo");
+        System.out.println("2 - Depositar dinheiro");
+        System.out.println("3 - Sacar dinheiro");
+        System.out.println("4 - Transferir dinheiro");
+        System.out.println("0 - Sair do sistema");
+
+    }
 
     }
 
